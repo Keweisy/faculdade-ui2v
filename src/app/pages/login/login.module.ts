942001f552ea8b7bace1ee4cd50e4e6818e2d2a1
Keweisy <dhominiakira0249@gmail.com>
2025-04-11 00:44:46 -0300

    alter 6

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,14 +1,16 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    FormsModule
-  ],
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ]
 })
-export class LoginModule { }
+export class LoginModule {}
