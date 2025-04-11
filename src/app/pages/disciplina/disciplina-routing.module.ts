77d94171a67571caf1d8e31a24a8c738f9007f89
Keweisy <dhominiakira0249@gmail.com>
2025-04-11 03:13:58 -0300

    alter 15

---
+++ src/app/pages/disciplina/disciplina-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DisciplinaListaComponent } from './disciplina-lista/disciplina-lista.component';
+import { DisciplinaCadastroComponent } from './disciplina-cadastro/disciplina-cadastro.component';
+
+const routes: Routes = [
+  { path: '', component: DisciplinaListaComponent },
+  { path: 'novo', component: DisciplinaCadastroComponent },
+  { path: ':id', component: DisciplinaCadastroComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DisciplinaRoutingModule {}
